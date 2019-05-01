cad7d30fb6787d303855a18c104c01904ed37d91
Angelo Pettinelli <angelo.pettinelli@icloud.com>
2019-05-01 16:33:00 +0200

    Updated with callback and comments. TO TEST.

--- libs/JoystickSubscriber/JoystickSubscriber.cpp
+++ libs/JoystickSubscriber/JoystickSubscriber.cpp
@@ -1,7 +1,14 @@
+/**
+ * @author pettinz
+ */
+
 #include "JoystickSubscriber.h"
 #include "json.hpp"
 
-namespace Politocean {
+#include "Commands.h"
+
+namespace Politocean
+{
 
 const std::string JoystickSubscriber::DFLT_ADDRESS   { "tcp://localhost:1883" };
 const std::string JoystickSubscriber::DFLT_CLIENT_ID { "JoystickSubscriber" };
@@ -9,14 +16,38 @@ const std::string JoystickSubscriber::DFLT_TOPIC     { "JoystickTopic" };
 
 void JoystickSubscriber::callback(const std::string& payload)
 {
-    // TODO: Elaborate payload, filter commands and send via SPI to microcontroller
-
     auto c_map = nlohmann::json::parse(payload);
     
-    axes_       = c_map["axes"].get<std::vector<int>>();
-    buttons_    = c_map["axes"].get<std::vector<int>>();
+    std::vector<int> axes       = c_map["axes"];
+    std::vector<int> buttons    = c_map["axes"];
 
-    std::cout << axes_[0] << std::endl;
+    axesBuffer_     = {
+        axes[COMMANDS::AXIS::X],
+        axes[COMMANDS::AXIS::Y],
+        axes[COMMANDS::AXIS::RZ]
+    };
+
+    buttonsBuffer_  = {
+        buttons[COMMANDS::BUTTONS::MOTORS_ON],
+        buttons[COMMANDS::BUTTONS::MOTORS_OFF],
+        buttons[COMMANDS::BUTTONS::VDOWN],
+        buttons[COMMANDS::BUTTONS::WRIST],
+        buttons[COMMANDS::BUTTONS::RESET],
+        buttons[COMMANDS::BUTTONS::VUP],
+        buttons[COMMANDS::BUTTONS::MEDIUM_FAST],
+        buttons[COMMANDS::BUTTONS::SLOW]
+    };
+
+    // Using shifts to convert a binary value vector to a decimal integer
+    int button = 0;
+    for (auto it = buttonsBuffer_.rbegin(); it != buttonsBuffer_.rend(); it++)
+        button += *it << (it - buttonsBuffer_.rbegin());
+
+    unsigned char data = (current_ >= axesBuffer_.size()) ? static_cast<unsigned char>(button) : axesBuffer_[current_];
+    sensors_[current_].setValue(controller_.SPIDataRW(data));
+
+    if (++current_ > sensors_.size())
+        current_ = 0;
 }
 
 }
--- libs/JoystickSubscriber/JoystickSubscriber.h
+++ libs/JoystickSubscriber/JoystickSubscriber.h
@@ -1,20 +1,66 @@
-#include "Subscriber.h"
+/**
+ * @author pettinz
+ */
 
+#include "Subscriber.h"
+#include "Sensor.h"
+#include "Controller.h"
+
+#include <string>
 #include <vector>
 
-namespace Politocean {
+namespace Politocean
+{
 
-class JoystickSubscriber : public Subscriber {
-    std::vector<int> axes_, buttons_;
+class JoystickSubscriber : public Subscriber
+{
+    Controller controller_;
+
+    /**
+     * @axesBuffer_     : contains the axes values to send via SPI
+     * @buttonsBuffer_  : contains the buttons values to send via SPI
+     */
+    std::vector<int> axesBuffer_, buttonsBuffer_;
+    std::vector<Sensor<unsigned char>> sensors_;
+
+    /**
+     * @current_: the index of the current value of the SPI buffer
+     */
+    int current_;
 
 public:
+    /**
+     * @DFLT_ADDRESS    : default mosquitto address
+     * @DFLT_CLIENT_ID  : default subscriber client id
+     * @DFLT_TOPIC      : default listening topic
+     */
     static const std::string DFLT_ADDRESS, DFLT_CLIENT_ID, DFLT_TOPIC;
 
+    /**
+     * Default constructor.
+     * It uses default values @DFLT_ADDRESS, @DFLT_CLIENT_ID, @DFLT_TOPIC
+     */
     JoystickSubscriber() : JoystickSubscriber(DFLT_ADDRESS, DFLT_CLIENT_ID, DFLT_TOPIC) {}
-
+    /**
+     * Constructor.
+     * 
+     * @address : mosquitto address
+     * @clientID: subscriber client id
+     * @topic   : listening topic
+     */
     JoystickSubscriber(const std::string& address, const std::string& clientID, const std::string& topic) 
-        : Subscriber(address, clientID, topic, &JoystickSubscriber::callback, this) {}
-
+        : Subscriber(address, clientID, topic, &JoystickSubscriber::callback, this), controller_(), current_(0)
+    {
+        for (auto sensor_type : sensor_t())
+            sensors_.emplace_back(Sensor<unsigned char>(sensor_type, 0));
+    }
+    
+    /**
+     * Callback function.
+     * It elaborates the joystick data (@payload) and use the SPI to send values and receive sensors data.
+     * 
+     * @payload: the string that recives from the JoystickPublisher
+     */
     void callback(const std::string& payload);
 };
 
