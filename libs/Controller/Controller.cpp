73761e75a6da0c4ecea7a2cf9567d70515559947
Angelo Pettinelli <angelo.pettinelli@icloud.com>
2019-05-08 20:08:48 +0200

    Update

--- libs/Controller/Controller.cpp
+++ libs/Controller/Controller.cpp
@@ -5,6 +5,9 @@
  */ 
 
 #include <iostream>
+#include <cstdlib>
+#include <thread>
+#include <chrono>
 
 #include "Controller.h"
 
@@ -13,20 +16,66 @@ namespace Politocean {
 #include "wiringPi.h"
 #include "wiringPiSPI.h"
 
-#include "unistd.h"
-
 void Controller::setup()
 {
     wiringPiSetupPhys();
-    wiringPiSPISetup(DEFAULT_SPI_CHANNEL, DEFAULT_SPI_SPEED);
-
-    pinMode(Constants::Pinout::MOTORS, OUTPUT);
-    pinMode(Constants::Pinout::RESET, OUTPUT);
-    
-    reset();
-
-    std::cout << "Controller is ready" << std::endl;
 }
+
+void Controller::setupSPI()
+{
+    wiringPiSPISetup(DEFAULT_SPI_CHANNEL, DEFAULT_SPI_SPEED);
+}
+
+void Controller::setupMotors()
+{
+    pinMode(Constants::Pinout::MOTORS, OUTPUT);
+}
+
+void Controller::setupArm()
+{
+    pinMode(Constants::Pinout::SHOULDER_EN, OUTPUT);
+    digitalWrite(Constants::Pinout::SHOULDER_EN, HIGH);
+
+    pinMode(Constants::Pinout::WRIST_EN, OUTPUT);
+    digitalWrite(Constants::Pinout::WRIST_EN, HIGH);
+}
+
+void Controller::set(Stepper stepper, bool value)
+{
+    int pin = getStepperEnable(stepper);
+
+    if (pin == -1)
+        std::exit(EXIT_FAILURE);
+
+    digitalWrite(pin, value);
+}
+
+void Controller::set(Stepper stepper, Direction direction)
+{
+    int pin = getStepperDirection(stepper);
+
+    if (pin == -1)
+        std::exit(EXIT_FAILURE);
+
+    digitalWrite(pin, static_cast<int>(direction));
+}
+
+void Controller::step(Stepper stepper)
+{
+    set(stepper, LOW);
+
+    int pin = getStepper(stepper);
+
+    if (pin == -1)
+        std::exit(EXIT_FAILURE);
+
+    digitalWrite(pin, LOW);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+    digitalWrite(pin, HIGH);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+}
+
 
 unsigned char Controller::SPIDataRW(unsigned char data)
 {
@@ -40,17 +89,75 @@ unsigned char Controller::SPIDataRW(unsigned char data)
 
 void Controller::reset()
 {
+    pinMode(Constants::Pinout::RESET, OUTPUT);
+
     digitalWrite(Constants::Pinout::RESET, LOW);
-    usleep(100);
+    std::this_thread::sleep_for(std::chrono::microseconds(100));
+
     digitalWrite(Constants::Pinout::RESET, HIGH);
-    sleep(3);
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+}
+
+void Controller::resetArm()
+{
+    pinMode(Constants::Pinout::SHOULDER_EN, INPUT);
+    pinMode(Constants::Pinout::SHOULDER_DIR, INPUT);
+    pinMode(Constants::Pinout::SHOULDER_STEPPER, INPUT);
 }
 
 void Controller::switchMotors()
 {
     motors_ = !motors_;
+
+    digitalWrite(Constants::Pinout::SHOULDER_EN, HIGH);
+    digitalWrite(Constants::Pinout::WRIST_EN, HIGH);
     
     digitalWrite(Constants::Pinout::MOTORS, motors_);
+}
+
+int Controller::getStepper(Stepper stepper)
+{
+    switch (stepper)
+    {
+        case Stepper::SHOULDER:
+            return Constants::Pinout::SHOULDER_STEPPER;
+        case Stepper::WRIST:
+            return Constants::Pinout::WRIST_STEPPER;
+        default:
+            break;
+    }
+
+    return -1;
+}
+
+int Controller::getStepperDirection(Stepper stepper)
+{
+    switch (stepper)
+    {
+        case Stepper::SHOULDER:
+            return Constants::Pinout::SHOULDER_DIR;
+        case Stepper::WRIST:
+            return Constants::Pinout::WRIST_STEPPER;
+        default:
+            break;
+    }
+
+    return -1;
+}
+
+int Controller::getStepperEnable(Stepper stepper)
+{
+    switch (stepper)
+    {
+        case Stepper::SHOULDER:
+            return Constants::Pinout::SHOULDER_EN;
+        case Stepper::WRIST:
+            return Constants::Pinout::WRIST_EN;
+        default:
+            break;
+    }
+
+    return -1;
 }
 
 }
--- libs/Controller/Controller.h
+++ libs/Controller/Controller.h
@@ -10,16 +10,33 @@
 namespace Politocean {
 
 class Controller {
-    bool motors_;
+public:
+    enum class Stepper { WRIST, SHOULDER };
+    enum class Direction { CCW, CW, NONE };
 
+private:
+    bool motors_ = false;
+
+    int getStepperEnable(Stepper stepper);
+    int getStepperDirection(Stepper stepper);
+    int getStepper(Stepper stepper);
 public:
     static const int DEFAULT_SPI_CHANNEL    = 0;
     static const int DEFAULT_SPI_SPEED      = 1000000;
 
-    Controller() : motors_(false) {}
+    Controller() {}
 
     // It sets up the controller
     void setup();
+
+    void setupSPI();
+    void setupMotors();
+    void setupArm();
+
+    void set(Stepper stepper, bool value);
+    void set(Stepper stepper, Direction direction);
+    
+    void step(Stepper stepper);
 
     /**
      * It returns the value read from SPI
@@ -32,6 +49,8 @@ public:
 
     // It resets the controller
     void reset();
+
+    void resetArm();
 };
 
 }
