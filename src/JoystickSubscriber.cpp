a920480a8fa2eaf084cd3789e332f46492818b81
Roberto Stagi <r.stagi96@gmail.com>
2019-05-10 22:42:34 +0200

    added 0xFF

--- src/JoystickSubscriber.cpp
+++ src/JoystickSubscriber.cpp
@@ -255,7 +255,6 @@ void SPI::startSPI(Controller& controller, Listener& listener)
 			std::vector<int> axes = listener.axes();
 
 			std::vector<unsigned char> buffer = {
-				(unsigned char) 0xFF,
 				(unsigned char) Politocean::map(axes[0],	SHRT_MIN, SHRT_MAX, 1, UCHAR_MAX-1),
 				(unsigned char) Politocean::map(axes[1],	SHRT_MIN, SHRT_MAX, 1, UCHAR_MAX-1),
 				(unsigned char) Politocean::map(axes[2],	SHRT_MIN, SHRT_MAX, 1, UCHAR_MAX-1)
@@ -320,6 +319,7 @@ void SPI::send(const std::vector<unsigned char>& buffer, Controller& controller,
 		if (data == 0xFF)
 		{
 			listener.resetCurrentSensor();
+			it = buffer.begin();
 			continue;
 		}
 		
