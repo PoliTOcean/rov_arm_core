875a6d89f2ef96a833bab4b555dc546c67c1a7e3
Roberto Stagi <r.stagi96@gmail.com>
2019-05-15 20:32:33 +0200

    fix new temperature

--- include/Commands.h
+++ include/Commands.h
@@ -15,7 +15,6 @@ namespace Commands {
         const string SHOULDER_OFF   = "SHOULDER_OFF";
         const string SHOULDER_UP    = "SHOULDER_UP";
         const string SHOULDER_DOWN  = "SHOULDER_DOWN";
-        const string SHOULDER_STOP  = "SHOULDER_STOP";
 
         const string WRIST_ON       = "WRIST_ON";
         const string WRIST_OFF      = "WRIST_OFF";
@@ -30,15 +29,10 @@ namespace Commands {
 
     namespace SPI
     {
-        const unsigned char MOTORS_SWAP        = 0x01;
-        const unsigned char MOTORS_ON          = 0x02;
-        const unsigned char MOTORS_OFF         = 0x03;
         const unsigned char VDOWN_ON           = 0x04;
         const unsigned char VDOWN_OFF          = 0x05;
         const unsigned char VUP_ON             = 0x06;
         const unsigned char VUP_OFF            = 0x07;
-        const unsigned char WRIST_SWAP         = 0x08;
-        const unsigned char RESET              = 0x0B;
         const unsigned char FAST               = 0x0D;
         const unsigned char SLOW               = 0x0E;
         const unsigned char MEDIUM             = 0x0C;
