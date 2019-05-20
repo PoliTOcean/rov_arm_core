6e0ade7dcfed91d43b6267233df0815a697ebb79
pettinz <angelo.pettinelli@icloud.com>
2019-05-20 17:05:40 +0200

    Velocity hand

--- src/Skeleton.cpp
+++ src/Skeleton.cpp
@@ -384,7 +384,7 @@ int main(int argc, const char *argv[])
         else if (action == Commands::Skeleton::HEAD_STEP)
         {
             head.setDirection(listener.headDirection());
-            head.setVelocity(1);
+            head.setVelocity(5);
             head.startStepping();
         }
         else if (action == Commands::Skeleton::HEAD_STOP)
