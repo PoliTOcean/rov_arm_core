89c7a162c8f05386ca80e5bae51f36627ef56fc1
Angelo Pettinelli <angelo.pettinelli@studenti.polito.it>
2020-02-27 20:03:09 +0100

    Z axis to send via SPI

--- include/Commands.h
+++ include/Commands.h
@@ -7,74 +7,76 @@
 
 using namespace std;
 
-namespace Politocean {
-namespace Constants {
-namespace Commands {
+namespace Politocean
+{
+namespace Constants
+{
+namespace Commands
+{
 
-    namespace ATMega
-    {
-        namespace Axes
-        {
-            const short X_AXIS      = 0;
-            const short Y_AXIS      = 1;
-            const short RZ_AXIS     = 2;
-            const short PITCH_AXIS  = 3;
-            const short UP_AXIS     = 4;
-            const short DOWN_AXIS   = 5;
-        }
+namespace ATMega
+{
+namespace Axes
+{
+const short X_AXIS = 0;
+const short Y_AXIS = 1;
+const short Z_AXIS = 2;
+const short RZ_AXIS = 3;
+const short PITCH_AXIS = 4;
+} // namespace Axes
 
-        namespace SPI
-        {
-            const unsigned char VDOWN_ON           = 0x04;
-            const unsigned char VDOWN_OFF          = 0x05;
-            const unsigned char VUP_ON             = 0x06;
-            const unsigned char VUP_OFF            = 0x07;
-            const unsigned char FAST               = 0x0D;
-            const unsigned char SLOW               = 0x0E;
-            const unsigned char MEDIUM             = 0x0C;
-            const unsigned char START_AND_STOP     = 0x12;
-            const unsigned char VUP_FAST_ON        = 0x13;
-            const unsigned char VUP_FAST_OFF       = 0x14;
-            const unsigned char PITCH_CONTROL      = 0x15;
+namespace SPI
+{
+const unsigned char VDOWN_ON = 0x04;
+const unsigned char VDOWN_OFF = 0x05;
+const unsigned char VUP_ON = 0x06;
+const unsigned char VUP_OFF = 0x07;
+const unsigned char FAST = 0x0D;
+const unsigned char SLOW = 0x0E;
+const unsigned char MEDIUM = 0x0C;
+const unsigned char START_AND_STOP = 0x12;
+const unsigned char VUP_FAST_ON = 0x13;
+const unsigned char VUP_FAST_OFF = 0x14;
+const unsigned char PITCH_CONTROL = 0x15;
 
-            namespace Delims
-            {
-                const unsigned char AXES         = 0xFF;
-                const unsigned char COMMAND      = 0x00;
-                const unsigned char SENSORS      = 0xFF;
-            }
-        }
-    }
+namespace Delims
+{
+const unsigned char AXES = 0xFF;
+const unsigned char COMMAND = 0x00;
+const unsigned char SENSORS = 0xFF;
+} // namespace Delims
+} // namespace SPI
+} // namespace ATMega
 
-    namespace Skeleton
-    {
-        const string SHOULDER_ON    = "SHOULDER_ON";
-        const string SHOULDER_OFF   = "SHOULDER_OFF";
-        const string SHOULDER_UP    = "SHOULDER_UP";
-        const string SHOULDER_DOWN  = "SHOULDER_DOWN";
-        const string SHOULDER_STEP  = "SHOULDER_STEP";
-        const string SHOULDER_STOP  = "SHOULDER_STOP";
+namespace Skeleton
+{
+const string SHOULDER_ON = "SHOULDER_ON";
+const string SHOULDER_OFF = "SHOULDER_OFF";
+const string SHOULDER_UP = "SHOULDER_UP";
+const string SHOULDER_DOWN = "SHOULDER_DOWN";
+const string SHOULDER_STEP = "SHOULDER_STEP";
+const string SHOULDER_STOP = "SHOULDER_STOP";
 
-        const string WRIST_ON       = "WRIST_ON";
-        const string WRIST_OFF      = "WRIST_OFF";
-        const string WRIST_START    = "WRIST_START";
-        const string WRIST_STOP     = "WRIST_STOP";
-        
-        const string HAND_START     = "HAND_START";
-        const string HAND_STOP      = "HAND_STOP";
+const string WRIST_ON = "WRIST_ON";
+const string WRIST_OFF = "WRIST_OFF";
+const string WRIST_START = "WRIST_START";
+const string WRIST_STOP = "WRIST_STOP";
 
-        const string HEAD_ON        = "HEAD_ON";
-        const string HEAD_OFF       = "HEAD_OFF";
-        const string HEAD_UP        = "HEAD_UP";
-        const string HEAD_DOWN      = "HEAD_DOWN";
-        const string HEAD_STEP      = "HEAD_STEP";
-        const string HEAD_STOP      = "HEAD_STOP";
+const string HAND_START = "HAND_START";
+const string HAND_STOP = "HAND_STOP";
 
-        const string NONE           = "NONE";
-    }
+const string HEAD_ON = "HEAD_ON";
+const string HEAD_OFF = "HEAD_OFF";
+const string HEAD_UP = "HEAD_UP";
+const string HEAD_DOWN = "HEAD_DOWN";
+const string HEAD_STEP = "HEAD_STEP";
+const string HEAD_STOP = "HEAD_STOP";
 
-}
-}
-}
+const string NONE = "NONE";
+} // namespace Skeleton
+
+} // namespace Commands
+} // namespace Constants
+} // namespace Politocean
 
 #endif //COMMANDS_H
--- src/ATMegaController.cpp
+++ src/ATMegaController.cpp
@@ -63,9 +63,8 @@ public:
 	// It setup class variables and sensors
 	Listener() : axes_(3, 0), axesUpdated_(false), commandsUpdated_(false), currentSensor_(sensor_t::First), sensorsUpdated_(false)
 	{
-		for(auto sensor_type : Politocean::sensor_t())
-			sensors_.emplace_back(Politocean::Sensor<float>(sensor_type,0));
-
+		for (auto sensor_type : Politocean::sensor_t())
+			sensors_.emplace_back(Politocean::Sensor<float>(sensor_type, 0));
 	}
 
 	// Returns the @axes_ vector
@@ -73,7 +72,7 @@ public:
 	// Returns the @button_ variable
 	std::string action();
 	// Returns the @sensor_ vector
-	Types::Vector<Sensor<float>>& sensors();
+	Types::Vector<Sensor<float>> &sensors();
 
 	/**
 	 * Callback functions.
@@ -85,7 +84,7 @@ public:
 	 * listenForAxes	: parses the string @payload into a JSON an stores the axes values inside @axes_ vector.
 	 */
 	void listenForAxes(Types::Vector<int> payload);
-	void listenForCommands(const std::string& payload);
+	void listenForCommands(const std::string &payload);
 	void listenForSensor(unsigned char data);
 
 	void resetCurrentSensor();
@@ -94,38 +93,38 @@ public:
 	bool isAxesUpdated();
 	bool isCommandsUpdated();
 	bool isSensorsUpdated();
-
 };
 
 void Listener::listenForAxes(Types::Vector<int> payload)
 {
-    mqttLogger::getInstance().log(logger::DEBUG, "Received axes: "+payload.stringify());
+	mqttLogger::getInstance().log(logger::DEBUG, "Received axes: " + payload.stringify());
 
 	axes_ = payload;
-	
+
 	axesUpdated_ = true;
 }
 
-void Listener::listenForCommands(const std::string& payload)
+void Listener::listenForCommands(const std::string &payload)
 {
-    mqttLogger::getInstance().log(logger::DEBUG, "Received command: "+payload);
+	mqttLogger::getInstance().log(logger::DEBUG, "Received command: " + payload);
 
-    commands_.push( payload );
+	commands_.push(payload);
 
 	commandsUpdated_ = true;
 }
 
 void Listener::listenForSensor(unsigned char data)
 {
-	if(currentSensor_ == sensor_t::ROLL || currentSensor_ == sensor_t::PITCH){
-		float f=(float)data;
+	if (currentSensor_ == sensor_t::ROLL || currentSensor_ == sensor_t::PITCH)
+	{
+		float f = (float)data;
 		f /= 10;
 		sensors_[static_cast<int>(currentSensor_)].setValue(f);
-	}else if(currentSensor_ == sensor_t::PRESSURE)
+	}
+	else if (currentSensor_ == sensor_t::PRESSURE)
 		sensors_[static_cast<int>(currentSensor_)].setValue(data + 990);
 	else
 		sensors_[static_cast<int>(currentSensor_)].setValue(data);
-	
 
 	if (++currentSensor_ > sensor_t::Last)
 		currentSensor_ = sensor_t::First;
@@ -147,14 +146,15 @@ Types::Vector<int> Listener::axes()
 std::string Listener::action()
 {
 	commandsUpdated_ = false;
-	if (commands_.empty()) return Commands::Actions::NONE;
+	if (commands_.empty())
+		return Commands::Actions::NONE;
 
 	string action = commands_.front();
 	commands_.pop();
 	return action;
 }
 
-Types::Vector<Sensor<float>>& Listener::sensors()
+Types::Vector<Sensor<float>> &Listener::sensors()
 {
 	return sensors_;
 }
@@ -186,16 +186,16 @@ class Talker
 public:
 	Talker() : isTalking_(false) {}
 
-	void startTalking(MqttClient& publisher, Listener& listener, Controller& controller);
+	void startTalking(MqttClient &publisher, Listener &listener, Controller &controller);
 	void stopTalking();
 
 	bool isTalking();
 };
 
-void Talker::startTalking(MqttClient& publisher, Listener& listener, Controller& controller)
+void Talker::startTalking(MqttClient &publisher, Listener &listener, Controller &controller)
 {
 	if (isTalking_)
-		return ;
+		return;
 
 	isTalking_ = true;
 	sensorThread_ = new std::thread([&]() {
@@ -204,7 +204,7 @@ void Talker::startTalking(MqttClient& publisher, Listener& listener, Controller&
 			if (!listener.isSensorsUpdated())
 			{
 				std::this_thread::sleep_for(std::chrono::seconds(Timing::Seconds::SENSORS));
-				continue ;
+				continue;
 			}
 			publisher.publish(Topics::SENSORS, listener.sensors());
 
@@ -216,8 +216,8 @@ void Talker::startTalking(MqttClient& publisher, Listener& listener, Controller&
 void Talker::stopTalking()
 {
 	if (!isTalking_)
-		return ;
-	
+		return;
+
 	isTalking_ = false;
 	sensorThread_->join();
 	actionsThread_->join();
@@ -228,27 +228,26 @@ bool Talker::isTalking()
 	return isTalking_;
 }
 
-
 /***************************************************
  * SPI Class
  **************************************************/
 
 class SPI
 {
-	Controller& controller_;
+	Controller &controller_;
 	std::mutex mutex_;
 
 	std::thread *SPIAxesThread_, *SPICommandsThread_;
 	bool isUsing_;
 
-	void send(const std::vector<unsigned char>& buffer, Listener &listener);
+	void send(const std::vector<unsigned char> &buffer, Listener &listener);
 
 public:
-	SPI(Controller& controller) : controller_(controller), isUsing_(false) {}
+	SPI(Controller &controller) : controller_(controller), isUsing_(false) {}
 
 	void setup();
 
-	void startSPI(Listener& listener, MqttClient& publisher);
+	void startSPI(Listener &listener, MqttClient &publisher);
 	void stopSPI();
 
 	bool isUsing();
@@ -261,43 +260,41 @@ void SPI::setup()
 
 unsigned char setAction(std::string action)
 {
-    if(action == Commands::Actions::ATMega::VDOWN_ON)
-        return Commands::ATMega::SPI::VDOWN_ON;
-    else if(action == Commands::Actions::ATMega::VDOWN_OFF)
-        return Commands::ATMega::SPI::VDOWN_OFF;
-    else if(action == Commands::Actions::ATMega::VUP_ON)
-        return Commands::ATMega::SPI::VUP_ON;
-    else if(action == Commands::Actions::ATMega::VUP_OFF)
-        return Commands::ATMega::SPI::VUP_OFF;
-	else if(action == Commands::Actions::ATMega::VUP_FAST_ON)
+	if (action == Commands::Actions::ATMega::VDOWN_ON)
+		return Commands::ATMega::SPI::VDOWN_ON;
+	else if (action == Commands::Actions::ATMega::VDOWN_OFF)
+		return Commands::ATMega::SPI::VDOWN_OFF;
+	else if (action == Commands::Actions::ATMega::VUP_ON)
+		return Commands::ATMega::SPI::VUP_ON;
+	else if (action == Commands::Actions::ATMega::VUP_OFF)
+		return Commands::ATMega::SPI::VUP_OFF;
+	else if (action == Commands::Actions::ATMega::VUP_FAST_ON)
 		return Commands::ATMega::SPI::VUP_FAST_ON;
-	else if(action == Commands::Actions::ATMega::VUP_FAST_OFF)
+	else if (action == Commands::Actions::ATMega::VUP_FAST_OFF)
 		return Commands::ATMega::SPI::VUP_FAST_OFF;
-    else if(action == Commands::Actions::ATMega::FAST)
-        return Commands::ATMega::SPI::FAST;
-    else if(action == Commands::Actions::ATMega::SLOW)
-        return Commands::ATMega::SPI::SLOW;
-    else if(action == Commands::Actions::ATMega::MEDIUM)
-        return Commands::ATMega::SPI::MEDIUM;
-    else if(action == Commands::Actions::ATMega::START_AND_STOP)
-        return Commands::ATMega::SPI::START_AND_STOP;
-    else if(action == Commands::Actions::ATMega::PITCH_CONTROL)
+	else if (action == Commands::Actions::ATMega::FAST)
+		return Commands::ATMega::SPI::FAST;
+	else if (action == Commands::Actions::ATMega::SLOW)
+		return Commands::ATMega::SPI::SLOW;
+	else if (action == Commands::Actions::ATMega::MEDIUM)
+		return Commands::ATMega::SPI::MEDIUM;
+	else if (action == Commands::Actions::ATMega::START_AND_STOP)
+		return Commands::ATMega::SPI::START_AND_STOP;
+	else if (action == Commands::Actions::ATMega::PITCH_CONTROL)
 		return Commands::ATMega::SPI::PITCH_CONTROL;
 	else
-        return 0;
+		return 0;
 }
 
-void SPI::startSPI(Listener& listener, MqttClient& publisher)
+void SPI::startSPI(Listener &listener, MqttClient &publisher)
 {
 	if (isUsing_)
-		return ;
+		return;
 
 	isUsing_ = true;
 
 	SPIAxesThread_ = new std::thread([&]() {
-
-		long long threshold = (Timing::Milliseconds::SENSORS_UPDATE_DELAY / Timing::Milliseconds::AXES_DELAY) 
-								/ ( static_cast<int>(sensor_t::Last) + 1 );
+		long long threshold = (Timing::Milliseconds::SENSORS_UPDATE_DELAY / Timing::Milliseconds::AXES_DELAY) / (static_cast<int>(sensor_t::Last) + 1);
 		int counter = 0;
 
 		while (isUsing_)
@@ -305,8 +302,9 @@ void SPI::startSPI(Listener& listener, MqttClient& publisher)
 			std::this_thread::sleep_for(std::chrono::milliseconds(Timing::Milliseconds::AXES_DELAY));
 
 			counter++;
-			
-			if(!listener.isAxesUpdated() && counter < threshold) continue;
+
+			if (!listener.isAxesUpdated() && counter < threshold)
+				continue;
 
 			Types::Vector<int> axes = listener.axes();
 
@@ -314,10 +312,9 @@ void SPI::startSPI(Listener& listener, MqttClient& publisher)
 				(unsigned char) Commands::ATMega::SPI::Delims::AXES,
 				(unsigned char) Politocean::map(axes[Commands::ATMega::Axes::X_AXIS],		SHRT_MIN, SHRT_MAX, 1, UCHAR_MAX-1),
 				(unsigned char) Politocean::map(axes[Commands::ATMega::Axes::Y_AXIS],		SHRT_MIN, SHRT_MAX, 1, UCHAR_MAX-1),
+				(unsigned char) Politocean::map(axes[Commands::ATMega::Axes::Z_AXIS]),		SHRT_MIN, SHRT_MAX, 1, UCHAR_MAX-1),
 				(unsigned char) Politocean::map(axes[Commands::ATMega::Axes::RZ_AXIS],		SHRT_MIN, SHRT_MAX, 1, UCHAR_MAX-1),
-				(unsigned char) Politocean::map(axes[Commands::ATMega::Axes::PITCH_AXIS],	SHRT_MIN, SHRT_MAX, 1, UCHAR_MAX-1),
-				(unsigned char) Politocean::map(axes[Commands::ATMega::Axes::UP_AXIS], 		SHRT_MIN, SHRT_MAX, 1, UCHAR_MAX-1),
-				(unsigned char) Politocean::map(axes[Commands::ATMega::Axes::DOWN_AXIS], 	SHRT_MIN, SHRT_MAX, 1, UCHAR_MAX-1)
+				(unsigned char) Politocean::map(axes[Commands::ATMega::Axes::PITCH_AXIS],	SHRT_MIN, SHRT_MAX, 1, UCHAR_MAX-1)
 			};
 
 			send(buffer, listener);
@@ -329,36 +326,36 @@ void SPI::startSPI(Listener& listener, MqttClient& publisher)
 	SPICommandsThread_ = new std::thread([&]() {
 		while (isUsing_)
 		{
-			if(!listener.isCommandsUpdated()){
-            	std::this_thread::sleep_for(std::chrono::milliseconds(Timing::Milliseconds::COMMANDS));
+			if (!listener.isCommandsUpdated())
+			{
+				std::this_thread::sleep_for(std::chrono::milliseconds(Timing::Milliseconds::COMMANDS));
 				continue;
 			}
 
 			std::string data = listener.action();
 
-    		mqttLogger::getInstance().log(logger::DEBUG, "Computing command: "+data);
-			
+			mqttLogger::getInstance().log(logger::DEBUG, "Computing command: " + data);
+
 			if (data == Commands::Actions::RESET)
 			{
-			    controller_.reset();
+				controller_.reset();
 			}
 			else if (data == Commands::Actions::ON)
-            {
-                ComponentsManager::SetComponentState(component_t::POWER, Component::Status::ENABLED);
-                controller_.startMotors();
-            }
-            else if (data == Commands::Actions::OFF)
-            {
-                ComponentsManager::SetComponentState(component_t::POWER, Component::Status::DISABLED);
-                controller_.stopMotors();
-            }
+			{
+				ComponentsManager::SetComponentState(component_t::POWER, Component::Status::ENABLED);
+				controller_.startMotors();
+			}
+			else if (data == Commands::Actions::OFF)
+			{
+				ComponentsManager::SetComponentState(component_t::POWER, Component::Status::DISABLED);
+				controller_.stopMotors();
+			}
 			else
-			{	// the command is for the spi
+			{ // the command is for the spi
 				unsigned char action = setAction(data);
 				std::vector<unsigned char> buffer = {
 					Commands::ATMega::SPI::Delims::COMMAND,
-					action
-				};
+					action};
 				send(buffer, listener);
 			}
 		}
@@ -368,13 +365,14 @@ void SPI::startSPI(Listener& listener, MqttClient& publisher)
 void SPI::stopSPI()
 {
 	if (!isUsing_)
-		return ;
+		return;
 
 	isUsing_ = false;
-	SPIAxesThread_->join(); SPICommandsThread_->join();
+	SPIAxesThread_->join();
+	SPICommandsThread_->join();
 }
 
-void SPI::send(const std::vector<unsigned char>& buffer, Listener& listener)
+void SPI::send(const std::vector<unsigned char> &buffer, Listener &listener)
 {
 	std::lock_guard<std::mutex> lock(mutex_);
 
@@ -387,7 +385,7 @@ void SPI::send(const std::vector<unsigned char>& buffer, Listener& listener)
 			listener.resetCurrentSensor();
 			continue;
 		}
-		
+
 		listener.listenForSensor(data);
 	}
 }
@@ -403,21 +401,20 @@ bool SPI::isUsing()
 
 int main(int argc, const char *argv[])
 {
-    mqttLogger::setRootTag(argv[0]);
-	
-	MqttClient& publisher = MqttClient::getInstance(Rov::ATMEGA_ID, Hmi::IP_ADDRESS);
+	mqttLogger::setRootTag(argv[0]);
+
+	MqttClient &publisher = MqttClient::getInstance(Rov::ATMEGA_ID, Hmi::IP_ADDRESS);
 
 	/**
 	 * @subscriber	: the subscriber listening to JoystickMqttClient topics
 	 * @listener	: object with the callbacks for @subscriber and methods to retreive data read
 	 */
-	MqttClient& subscriber = MqttClient::getInstance(Rov::ATMEGA_ID, Rov::IP_ADDRESS);
+	MqttClient &subscriber = MqttClient::getInstance(Rov::ATMEGA_ID, Rov::IP_ADDRESS);
 	Listener listener;
 
 	// Subscribe @subscriber to joystick publisher topics
-	subscriber.subscribeTo(Topics::AXES, 			&Listener::listenForAxes, 		&listener);
-	subscriber.subscribeTo(Topics::COMMANDS,		&Listener::listenForCommands, 	&listener);
-
+	subscriber.subscribeTo(Topics::AXES, &Listener::listenForAxes, &listener);
+	subscriber.subscribeTo(Topics::COMMANDS, &Listener::listenForCommands, &listener);
 
 	/**
 	 * @controller : to access to Raspberry Pi features
@@ -426,7 +423,7 @@ int main(int argc, const char *argv[])
 	Controller controller;
 
 	ComponentsManager::Init(Rov::ATMEGA_ID);
-	
+
 	// Try to setup @controller
 	try
 	{
@@ -450,7 +447,6 @@ int main(int argc, const char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-
 	SPI spi(controller);
 
 	// Try to setup @spi
@@ -458,17 +454,17 @@ int main(int argc, const char *argv[])
 	{
 		spi.setup();
 	}
-	catch(const std::exception& e)
+	catch (const std::exception &e)
 	{
 		mqttLogger::getInstance().log(logger::ERROR, "Can't setup SPI!", e);
 		exit(EXIT_FAILURE);
 	}
-	catch(...)
+	catch (...)
 	{
 		mqttLogger::getInstance().log(logger::ERROR, "Can't setup SPI!");
 		exit(EXIT_FAILURE);
 	}
-	
+
 	spi.startSPI(listener, publisher);
 
 	mqttLogger::getInstance().log(logger::CONFIG, "SPI initialized");
@@ -485,6 +481,6 @@ int main(int argc, const char *argv[])
 
 	//safe reset at the end
 	controller.reset();
-	
+
 	return 0;
 }
